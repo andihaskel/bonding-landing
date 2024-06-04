858a87ec6d80faa0ee11263a8b6f5beab29bb413
Andrés Haskel <andres.haskel@gmail.com>
2024-06-03 23:02:38 -0300

    feat: animation

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,27 +1,26 @@
-import React, { useState } from 'react'
-import { delay, motion } from 'framer-motion'
+import { motion } from 'framer-motion';
+// import React from 'react';
+import Lottie from 'lottie-react';
 import Container from 'react-bootstrap/Container';
-import './hero.scss'
-// import HeroImg from '/andib-hero.svg'
-import HeroBigImg from '/hero-big-img.svg'
-import HtmlIcon from '/html-icon.svg'
-import FlutterIcon from '/skill-icons_flutter-dark.svg'
-import NodeIconOne from '/skill-icons_nodejs-dark.svg'
-import NodeIconTwo from '/skill-icons_nodejs-dark-two.svg'
-import NodeIconThree from '/skill-icons_nodejs-dark-three.svg'
-import NodeIconFour from '/skill-icons_nodejs-dark-four.svg'
-import BigTaskIcon from '/big-task-icon.svg'
-import MugIcon from '/mug.svg'
+import './hero.scss';
+// import HeroBigImg from '/hero-big-img.svg';
+import heroAnimation from './hero-animation.json';
+import BigTaskIcon from '/big-task-icon.svg';
+import HtmlIcon from '/html-icon.svg';
+import FlutterIcon from '/skill-icons_flutter-dark.svg';
+// import NodeIconFour from '/skill-icons_nodejs-dark-four.svg';
+import NodeIconThree from '/skill-icons_nodejs-dark-three.svg';
+import NodeIconTwo from '/skill-icons_nodejs-dark-two.svg';
+import NodeIconOne from '/skill-icons_nodejs-dark.svg';
 
 
 const Hero = () => {
-  
   const imageVariants = {
     initial: {
       opacity: 0,
       scale: 0.5,
     },
-    animate: (i) => ( {
+    animate: (i) => ({
       opacity: 1,
       scale: 1,
       transition: {
@@ -33,26 +32,31 @@ const Hero = () => {
   }
 
   const items = [
-    { className: 'hero_img htmlIcon', src: HtmlIcon  },
+    { className: 'hero_img htmlIcon', src: HtmlIcon },
     { className: 'hero_img flutterIcon', src: FlutterIcon },
     { className: 'hero_img nodeIconOne', src: NodeIconOne },
     { className: 'hero_img nodeIconTwo', src: NodeIconTwo },
     { className: 'hero_img nodeIconThree', src: NodeIconThree },
-    { className: 'hero_img nodeIconFour', src: NodeIconFour },
+    // { className: 'hero_img nodeIconFour', src: NodeIconFour },
     { className: 'hero_img nodeIconFive', src: BigTaskIcon },
   ]
 
   return (
     <div className='hero'>
       <Container fluid>
-        <motion.div className="main_row row">
+        <motion.div className="main_row row" style={{
+          display: 'flex',
+          // flex: 1,
+          alignItems: 'center', /* Alinea verticalmente */
+          // justifyContent: 'center', /* Alinea horizontalmente (opcional) */
+        }}>
           <motion.div className="wrapper_title_container col-lg-6 col-xl-7">
             <div className='title-container'>
-              <motion.h1>Strong Bonds, <br/> Stronger Products  </motion.h1>
+              <motion.h1>Strong Bonds, <br /> Stronger Products  </motion.h1>
               <motion.p>Our nearshore software development teams deliver user-oriented design, top-level development, and scalable technology to bring your idea to life.</motion.p>
               <ul className="btn-container d-flex align-items-center">
                 <motion.li>
-                  <motion.a 
+                  <motion.a
                     href="#Contact"
                     className='btn'
                   >
@@ -60,9 +64,9 @@ const Hero = () => {
                   </motion.a>
                 </motion.li>
                 <motion.li>
-                  <motion.a 
+                  <motion.a
                     href="#WhatWeDo"
-                    className='btn secondary' 
+                    className='btn secondary'
                   >
                     Our Services
                   </motion.a>
@@ -70,27 +74,41 @@ const Hero = () => {
               </ul>
             </div>
           </motion.div>
-          <div className="col-lg-6 col-xl-5">
-            <div className='img-container'>
-              <motion.div 
-                className='inner_container'
-                initial="initial" animate="animate"
-              >
-                <motion.img className='bigIcon' src={HeroBigImg} alt="" />
+          <div className="col-lg-6 col-xl-5 right-container" id={'div-1'}>
+            <div id={'right-container-1'} style={{
+              height: '100%',
+              display: 'flex', /* Usa flexbox */
+              alignItems: 'center', /* Alinea verticalmente */
+              justifyContent: 'center', /* Alinea horizontalmente */
+            }}>
+              <Lottie width={100} height={100} animationData={heroAnimation} loop={true} />
+            </div>
+            <div style={{
+              position: 'absolute',
+              height: '100%',
+              width: '100%',
+              display: 'flex', /* Usa flexbox */
+              alignItems: 'center', /* Alinea verticalmente */
+              justifyContent: 'center', /* Alinea horizontalmente */
+            }}>
+              <div id={'right-container-2'} style={{
+                position: 'relative',
+                height: '100%',
+                width: '100%',
+                // maxHeight: 440,
+              }}>
 
-                {items.map((item, i) => (
-                  <motion.img className={item.className} src={item.src} variants={imageVariants} custom={i} key={i} />
-                ))}
+                <motion.div
+                  className='inner_container'
+                  initial="initial" animate="animate"
+                >
+                  {/* <motion.img className='bigIcon' src={HeroBigImg} alt="" /> */}
 
-                {/* <motion.img className='hero_img mugIcon' src={MugIcon} alt="" variants={imageVariants} /> */}
-                {/* <motion.img className='hero_img htmlIcon' src={HtmlIcon} alt="" variants={imageVariants} custom={i} />
-                <motion.img className='hero_img flutterIcon' src={FlutterIcon} alt="" variants={imageVariants} custom={i} />
-                <motion.img className='hero_img nodeIconOne' src={NodeIconOne} alt="" variants={imageVariants} custom={i} />
-                <motion.img className='hero_img nodeIconTwo' src={NodeIconTwo} alt="" variants={imageVariants} custom={i} />
-                <motion.img className='hero_img nodeIconThree' src={NodeIconThree} alt="" variants={imageVariants} custom={i} />
-                <motion.img className='hero_img nodeIconFour' src={NodeIconFour} alt="" variants={imageVariants} custom={i} />
-                <motion.img className='hero_img nodeIconFive' src={BigTaskIcon} alt="" variants={imageVariants} custom={i} /> */}
-              </motion.div>
+                  {items.map((item, i) => (
+                    <motion.img className={item.className} src={item.src} variants={imageVariants} custom={i} key={i} />
+                  ))}
+                </motion.div>
+              </div>
             </div>
           </div>
         </motion.div>
